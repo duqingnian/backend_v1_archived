7bdf5fb94413bef57efc669835241bfc4eabf841
duqingnian <duqingnian@163.com>
2023-09-02 22:45:51 +0800

    所有没有开发完的功能更新提示为维护中

---
+++ src/FrontBundle/Controller/TixianController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FrontBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TixianController extends \AppBundle\Controller\BaseController
+{
+	public function indexAction(Request $request)
+    {
+        $action = $request->query->get("action",'index');
+        $method = '_'.$action;
+        if(!method_exists($this,$method))
+        {
+            echo json_encode(['code'=>-1,'msg'=>'method:'.$action.' not exist!']);
+            exit();
+        }
+		return $this->{$method}($request);
+    }
+    
+    private function _index($request)
+    {
+		return $this->render('FrontBundle:Tixian:index.html.twig',$this->data);
+	}
+}
+
+
