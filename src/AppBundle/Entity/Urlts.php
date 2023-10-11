fd5f1605b9aaa5ff95ff61934942b531e28aa543
duqingnian <duqingnian@163.com>
2023-10-12 02:41:35 +0800

    python script auto upload this file.

---
+++ src/AppBundle/Entity/Urlts.php
@@ -0,0 +1,57 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_url_ts")
+ */
+class Urlts
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+	/**
+     * @ORM\Column(type="string", length=300)
+     */
+    private $url="";
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Urlts
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
