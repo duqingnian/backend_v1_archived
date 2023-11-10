d459fe2fff4ed03d72ce1e57bfe506b485ccc9ef
duqingnian <duqingnian@163.com>
2023-11-10 12:18:50 +0800

    python script auto upload this file.

---
+++ src/AppBundle/Entity/NotifyOriginalData.php
@@ -0,0 +1,86 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_notify_original_data")
+ */
+class NotifyOriginalData
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+	
+	/**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $bundle = ""; //PAYIN  PAYOUT
+	
+	/**
+     * @ORM\Column(type="text")
+     */
+    private $data = "";
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set bundle
+     *
+     * @param string $bundle
+     *
+     * @return NotifyOriginalData
+     */
+    public function setBundle($bundle)
+    {
+        $this->bundle = $bundle;
+
+        return $this;
+    }
+
+    /**
+     * Get bundle
+     *
+     * @return string
+     */
+    public function getBundle()
+    {
+        return $this->bundle;
+    }
+
+    /**
+     * Set data
+     *
+     * @param string $data
+     *
+     * @return NotifyOriginalData
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data
+     *
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+}
